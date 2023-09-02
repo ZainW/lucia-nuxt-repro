09c4e7ec1803c7f14ed0baa9b9753ad860f13094
Zain Wania <zain@wania.dev>
2023-09-02 10:41:00 -0400

    turso works

--- server/utils/lucia.ts
+++ server/utils/lucia.ts
@@ -1,17 +1,18 @@
 import { lucia } from "lucia";
 import { github } from "@lucia-auth/oauth/providers";
-import { betterSqlite3 } from "@lucia-auth/adapter-sqlite";
+import { libsql } from "@lucia-auth/adapter-sqlite";
 import { h3 } from "lucia/middleware";
+import { createClient } from "@libsql/client";
 // import "lucia/polyfill/node";
 
-import sqlite from "better-sqlite3";
-import fs from "fs";
 
-const db = sqlite(":memory:");
-db.exec(fs.readFileSync("schema.sql", "utf8"));
+const db = createClient({
+	url: 'http://127.0.0.1:8080'
+});
+
 
 export const auth = lucia({
-	adapter: betterSqlite3(db, {
+	adapter: libsql(db, {
 		user: "user",
 		session: "user_session",
 		key: "user_key"
